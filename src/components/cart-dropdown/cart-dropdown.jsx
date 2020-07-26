c7278e9fec1ceb96581d1851fd0c3387c854bd84
prashantchanne12 <prashantchanne121@gmail.com>
2020-07-26 19:12:02 +0530

    added reselector

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart-selectors.js";
 
 import CustomButton from "../custom-button/custom-button.jsx";
 import CartItem from "../cart-item/cart-item.jsx";
@@ -18,7 +19,7 @@ const CartDropdown = ({ cartItems }) => (
 );
 
 const mapStateToProps = (state) => ({
-  cartItems: state.cart.cartItems,
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps, null)(CartDropdown);
---
+++ src/redux/cart/cart-selectors.js
@@ -0,0 +1,16 @@
+import { createSelector } from 'reselect';
+
+
+// ------ INPUT SELECTOR ---------
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0),
+);
